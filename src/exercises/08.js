97e81059686e464b9a9cb0c04b69f81964508f95
Abdul Khan <x210812@L600045.home>
2018-07-22 19:49:51 -0400

    state reducer

--- src/exercises/08.js
+++ src/exercises/08.js
@@ -27,11 +27,18 @@ class Toggle extends React.Component {
   static defaultProps = {
     initialOn: false,
     onReset: () => {},
-    // 🐨 let's add a default stateReducer here. It should return
-    // the changes object as it is passed.
+    stateReducer: (state, changes) => ({...changes})
   }
   initialState = {on: this.props.initialOn}
   state = this.initialState
+  internalSetState = (changes, callback) => {
+    this.setState(currentState => {
+      return [changes]
+        .map(c => typeof changes === 'function' ? c(currentState): c)
+        .map(c => this.props.stateReducer(currentState, c) || {})
+        .map(c => Object.keys(c).length ? c : null)[0];
+    }, callback)
+  }
   // 🐨 let's add a method here called `internalSetState`. It will simulate
   // the same API as `setState(updater, callback)`:
   // - updater: (changes object or function that returns the changes object)
@@ -49,11 +56,11 @@ class Toggle extends React.Component {
   // 🐨 Finally, update all pre-existing instances of this.setState
   // to this.internalSetState
   reset = () =>
-    this.setState(this.initialState, () =>
+    this.internalSetState(this.initialState, () =>
       this.props.onReset(this.state.on),
     )
   toggle = () =>
-    this.setState(
+    this.internalSetState(
       ({on}) => ({on: !on}),
       () => this.props.onToggle(this.state.on),
     )
